75b1e74fa72035e8084c8ebb262ce845f109af00
Dartagnan <solucoesconsultoria542@gmail.com>
2025-08-12 15:01:25 -0300

    set up dynamic route

---
+++ src/routes/products/+page.svelte
@@ -0,0 +1,4 @@
+<h1>LIST OF PRODUCTS</h1>
+<a href="/products/1"><h1>Product 1</h1></a>
+<a href="/products/2"><h1>Product 2</h1></a>
+<a href="/products/3"><h1>Product 3</h1></a>
---
+++ src/routes/products/[id]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	//coletar id via params
+	//armazenar em variável -> id
+	//passar a variavel id no html abaixo como product {id}
+</script>
+
+<h1>Product</h1>
